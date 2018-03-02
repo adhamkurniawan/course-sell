d79f51e4b52864a88499701172d64e7b0af1a557
Adham Kurniawan <adham.kurniawan@crowdo.com>
2018-03-02 19:11:16 +0700

    no changed

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,5 @@
 class CoursesController < ApplicationController
-  protect_from_forgery except: [:notify]
+  protect_from_forgery except: [:payment_notification]
 
   def index
     @courses = Course.all
