c2a8a6725ac75d54191b5abd1b432921e5cb5946
Adham Kurniawan <adham.kurniawan@crowdo.com>
2018-03-02 20:51:02 +0700

    create seo url with friendly id

--- app/admin/tasks.rb
+++ app/admin/tasks.rb
@@ -1,6 +1,12 @@
 ActiveAdmin.register Task do
   permit_params :course_id, :title, :description, :video_url, :image, :preview
 
+  controller do
+    def find_resource
+      scoped_collection.friendly.find(params[:id])
+    end
+  end
+
   index do
     selectable_column
     column :id
