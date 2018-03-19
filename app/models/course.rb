9f8060d3d34e726e96c341113cd9c7c97f56e548
Adham Kurniawan <adham.kurniawan@crowdo.com>
2018-03-19 08:45:53 +0700

    added reviews

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,6 +4,7 @@ class Course < ApplicationRecord
 
   has_many :tasks, dependent: :destroy
   has_many :subscriptions, dependent: :destroy
+  has_many :reviews, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 50 }
   validates :description, presence: true
@@ -11,4 +12,8 @@ class Course < ApplicationRecord
 
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  def average_rating
+    reviews.blank? ? 0 : reviews.average(:star).round(2)
+  end
 end
